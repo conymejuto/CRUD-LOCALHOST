7ab279f287673634032b99fc83721c25d107bc61
conymejuto <conymejuto@gmail.com>
2021-03-18 21:09:28 -0300

    agrego index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const formularioForm = document.getElementById("formulario");
+const emailInput = document.getElementById("inputEmail");
+const passInput = document.getElementById("inputPass");
+const nombreInput = document.getElementById("inputNombre");
+const rolInput = document.getElementById("inputRol");
+const json = localStorage.getItem("usuarios"); // Traer de localStorage el dato asociado a la key usuarios.
+
+const data = JSON.parse(json); // Convertir datos de un string JSON a código JavaScript.
+const usuarios = data || [];
+
+formularioForm.onsubmit = function (e) {
+  //funcion que guarda en json lo que se ingresa en el form
+  e.preventDefault();
+  const usuario = {
+    email: emailInput.value,
+    pass: passInput.value,
+    nombre: nombreInput.value,
+    rol: rolInput.value,
+  };
+  usuarios.push(usuario);
+  const json = JSON.stringify(usuarios); // Convertir datos a un string JSON.
+  localStorage.setItem("usuarios", json); // Guardar en localStorage un dato asociado a la key "usuarios".
+};
